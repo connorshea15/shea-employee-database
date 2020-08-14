4d0500f0a3a4775ec55da7377ec45fbba62d6da0
connorshea15 <connorshea15@gmail.com>
2020-08-13 18:06:11 -0700

    Got my schema working for all tables!

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
+USE company_db;
 
+CREATE TABLE departments(
+    id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles(
+    id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    deparment_id INTEGER(11),
+    CONSTRAINT fk_department FOREIGN KEY (deparment_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees(
+    id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    manager_id INTEGER(11), 
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
