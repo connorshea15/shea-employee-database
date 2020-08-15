5905dc376f9f7749a391e4d5f68db0d20806a855
connorshea15 <connorshea15@gmail.com>
2020-08-15 16:51:35 -0700

    I am inquiring and showing departments, roles, and employees

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 const connection =require('./db/database');
 
   connection.connect(err => {
@@ -7,7 +8,32 @@ const connection =require('./db/database');
     afterConnection();
   });
   
-  afterConnection = () => {
+// Function to show all departments
+showAllDepartments = () => {
+    connection.query(`SELECT * FROM departments`, 
+        function(err, res) {
+            if (err) throw err;
+            console.log(console.table(res));
+            //connection.end();
+            afterConnection();
+        });
+};
+
+// Function to show all roles
+showAllRoles = () => {
+    connection.query(`SELECT roles.*, departments.name AS department_name
+                        FROM roles
+                        LEFT JOIN departments ON roles.department_id = departments.id;`, 
+        function(err, res) {
+            if (err) throw err;
+            console.log(console.table(res));
+            //connection.end();
+            afterConnection();
+        });
+};
+
+// Function to show all employees
+showAllEmployees = () => {
     connection.query(`SELECT e.id, e.first_name, e.last_name, 
                         roles.title AS title, 
                         departments.name AS department, 
@@ -20,6 +46,37 @@ const connection =require('./db/database');
         function(err, res) {
             if (err) throw err;
             console.log(console.table(res));
-            connection.end();
+            //connection.end();
+            afterConnection();
         });
+};
+
+
+  afterConnection = () => {
+    
+    // we will inquire about what this person wants to do
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'start',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments!',
+            'View all roles!',
+            'View all employees!',
+            'Add a department!',
+            'Add a role!',
+            'Add an employee!',
+            'Update and employee role!'
+        ]
+        }
+    ]).then(answer => {
+        if(answer.start === 'View all departments!') {
+            return showAllDepartments();
+        } else if (answer.start === 'View all roles!') {
+            return showAllRoles();
+        } else if (answer.start === 'View all employees!') {
+            return showAllEmployees();
+        }
+    });
   };
